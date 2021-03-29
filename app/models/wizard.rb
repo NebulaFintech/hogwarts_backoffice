07105275358860be9fbd1938a40c33f65123fb51
Mauricio Murga <murga.mauricio@gmail.com>
2021-03-29 12:21:55 -0600

    add front

--- app/models/wizard.rb
+++ app/models/wizard.rb
@@ -1,9 +1,14 @@
 class Wizard < ApplicationRecord
   belongs_to :house, optional: true
+  has_many :school_applications
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   enum role: %i[staff student]
+
+  def full_name
+    names + ' ' + surname
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  resources :houses, only: [:index] do
+    member do
+      get :apply
+    end
+  end
+  resources :school_applications, only: [:index, :show] do
+    member do
+      get :approve
+      get :deny
+    end
+  end
+  
+  root 'houses#index'
   devise_for :wizards
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
